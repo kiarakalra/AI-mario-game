f4c3990d1b29b2407d36479e423a49d1a1bc4096
kiarakalra <71638118+kiarakalra@users.noreply.github.com>
2020-11-21 12:23:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
-	setSprites();
-	MarioAnimation();
+	
 	Mario_gameover = loadSound("gameover.wav");
 	mario_jump = loadSound("jump.wav");
 	mario_coin = loadSound("coin.wav");
 	mario_kick = loadSound("kick.wav");
 	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
 }
 
 function setup() {
@@ -17,8 +18,8 @@ function setup() {
 	video.size(800,400);
 	video.parent('game_console');
 
-	posenet = ml5.posenet(video,modelLoaded);
-	posenet.on('pose',gotposes);
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
 }
 
 function modelLoaded() 
@@ -26,18 +27,18 @@ function modelLoaded()
 	console.log("modelLoaded")
 }
 
-function gotResults(results) 
+function gotPoses(results) 
 {
 	if (results.length > 0) 
 	{
-	   console.log("results");
+	   console.log(results);
 	   noseX = results[0].pose.nose.x;
 	   noseY = results[0].pose.nose.y;	
 	}
 }
 
 function draw() {
-	game()
+	game();
 }
 
 
